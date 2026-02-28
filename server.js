633c3304d21b4da633fa9fc3edd990943c83ca59
Raj Lakhera <rajlakhera.dev@gmail.com>
2026-02-28 12:17:30 +0530

    new chnages

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(express.static(__dirname));
 
 // Serve the game page
 app.get('/game', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'game.html'));
 });
 
 // Fallback: Send login page for root URL
